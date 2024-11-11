9b30e3823e8b7c71ab6cfa27fdda1e89d71d35a8
lucxisto <lucasmxisto@outlook.com>
2024-11-11 18:13:06 -0300

    componentizando

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 const Header = (props: any) => {
     const { name, surname } = props;
     return(
-        <header className="flex justify-evenly items-center bg-zinc-300 font-bold h-[100px]">
+        <header className="flex justify-evenly items-center bg-zinc-300 font-bold h-[100px] py-20">
             <h1>Hello World!</h1>
             <img src="/logo.png" alt="logo" className="h-40 bg-black rounded-[100%]" />
             <h2>Welcome Mr. {`${surname}, ${name}`}</h2>

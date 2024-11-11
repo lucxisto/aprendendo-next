dcd5dfd67a4dc5001f8d9bea7a83f29d39dbf536
lucxisto <lucasmxisto@outlook.com>
2024-11-11 19:00:30 -0300

    criando componentes de pedidos

---
+++ src/components/OrderCard.tsx
@@ -0,0 +1,21 @@
+interface Props {
+    customerName: string;
+    totalOrderValue: number;
+    isPaid: boolean;
+}
+
+const OrderCard = (props: Props) => {
+    const { customerName, totalOrderValue, isPaid } = props;
+    return (
+        <div className="flex flex-col items-center justify-evenly border-zinc-900 border-2 p-2 rounded">
+            <h1>{customerName}</h1>
+            <p>Valor Total: R$ {totalOrderValue}</p>
+            {   isPaid?  (
+                    <p>PAGO</p>
+                ) : (<p>PENDENTE</p>)
+            }
+            <button>Editar Pedido</button>
+        </div>
+    );
+};
+export default OrderCard;

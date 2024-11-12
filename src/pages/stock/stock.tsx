b950e7c0b86946a9bc622857bb15dd10e63d11ef
lucxisto <lucasmxisto@outlook.com>
2024-11-12 14:03:50 -0300

    navegação de menu implementada

---
+++ src/pages/stock/stock.tsx
@@ -0,0 +1,42 @@
+import Header from "@/components/Header";
+
+const stock = () => {
+    return(
+        <>
+            <Header name="" surname=""/>
+            <div>
+                <div>
+                    <h1>Coca-cola</h1>
+                    <img src="https://placehold.co/100" alt="" />
+                    <p>quantidade: x</p>
+                    <p>preço: R$ 6,00</p>
+                </div>
+                <div>
+                    <h1>Coca-cola</h1>
+                    <img src="https://placehold.co/100" alt="" />
+                    <p>quantidade: x</p>
+                    <p>preço: R$ 6,00</p>
+                </div>
+                <div>
+                    <h1>Coca-cola</h1>
+                    <img src="https://placehold.co/100" alt="" />
+                    <p>quantidade: x</p>
+                    <p>preço: R$ 6,00</p>
+                </div>
+                <div>
+                    <h1>Coca-cola</h1>
+                    <img src="https://placehold.co/100" alt="" />
+                    <p>quantidade: x</p>
+                    <p>preço: R$ 6,00</p>
+                </div>
+                <div>
+                    <h1>Coca-cola</h1>
+                    <img src="https://placehold.co/100" alt="" />
+                    <p>quantidade: x</p>
+                    <p>preço: R$ 6,00</p>
+                </div>
+            </div>
+        </>
+    );
+};
+export default stock;

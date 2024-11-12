50af71f3ad55b620daed6875b61f7952d6d13bc0
lucxisto <lucasmxisto@outlook.com>
2024-11-12 13:53:30 -0300

    criando a parte de estoque de produtos

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 interface Props {
     name: string;
     surname: string;
@@ -6,10 +7,18 @@ interface Props {
 const Header = (props: Props) => {
     const { name, surname } = props;
     return(
-        <header className="flex justify-evenly items-center bg-zinc-300 font-bold h-[100px] py-20">
-            <h1>Hello World!</h1>
-            <img src="/logo.png" alt="logo" className="h-40 bg-black rounded-[100%]" />
-            <h2>Welcome Mr. {`${surname}, ${name}`}</h2>
+        <header >
+            <div className="flex justify-evenly items-center bg-zinc-300 font-bold h-[100px] py-20">
+                <h1>Hello World!</h1>
+                <img src="/logo.png" alt="logo" className="h-40 bg-black rounded-[100%]" />
+                <h2>Welcome Mr. {`${surname}, ${name}`}</h2>
+            </div>
+            <div className="flex justify-evenly items-center">
+                <Link href={"/"}>HOME</Link>
+                <Link href={"/orders/orders"}>PEDIDOS</Link>
+                <h3>PEDIDOS</h3>
+                
+            </div>
         </header>
    );
 };

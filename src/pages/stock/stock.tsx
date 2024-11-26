750cb681151b998ebc96d34855c01b97abf06b90
lucxisto <lucasmxisto@outlook.com>
2024-11-25 21:25:15 -0300

    adicionando state

--- src/pages/stock/stock.tsx
+++ src/pages/stock/stock.tsx
@@ -1,7 +1,20 @@
 import Header from "@/components/Header";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 const stock = () => {
+    const [quantity, setQuantity] = useState(0);
+
+    const increaseQuantity = () =>{
+        const counter = quantity + 1;
+        setQuantity(counter)
+    }
+
+    const decreaseQuantity = () =>{
+        const counter = quantity - 1;
+        setQuantity(counter)
+    }
+
     return(
         <>
             <Header />
@@ -9,33 +22,17 @@ const stock = () => {
                 <div>
                     <h1>Coca-cola</h1>
                     <img src="https://placehold.co/100" alt="" />
-                    <p>quantidade: x</p>
+                    <p>quantidade:
+                        <button onClick={decreaseQuantity}>
+                            -
+                        </button> 
+                        {quantity} 
+                        <button onClick={increaseQuantity}>
+                            +
+                        </button></p>
                     <p>preço: R$ 6,00</p>
                 </div>
-                <div>
-                    <h1>Coca-cola</h1>
-                    <img src="https://placehold.co/100" alt="" />
-                    <p>quantidade: x</p>
-                    <p>preço: R$ 6,00</p>
-                </div>
-                <div>
-                    <h1>Coca-cola</h1>
-                    <img src="https://placehold.co/100" alt="" />
-                    <p>quantidade: x</p>
-                    <p>preço: R$ 6,00</p>
-                </div>
-                <div>
-                    <h1>Coca-cola</h1>
-                    <img src="https://placehold.co/100" alt="" />
-                    <p>quantidade: x</p>
-                    <p>preço: R$ 6,00</p>
-                </div>
-                <div>
-                    <h1>Coca-cola</h1>
-                    <img src="https://placehold.co/100" alt="" />
-                    <p>quantidade: x</p>
-                    <p>preço: R$ 6,00</p>
-                </div>
+            
             </div>
         </>
     );
